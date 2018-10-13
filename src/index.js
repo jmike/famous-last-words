defb6236fa8027a0da79869ad45f68187cc364aa
Dimitrios C. Michalakos <dimitris@jmike.gr>
2018-10-13 18:34:18 +0300

    Add some popular quotes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default [
+  "You don't need tests; nobody got laid because of tests.",
+  'When in doubt, use a popup.',
+  'Let’s debug it directly in production.',
+  'Just ship it already.',
+];
