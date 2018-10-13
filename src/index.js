9d252f8616b15156c4d244e0ccd943e9ada5edcb
George Avgoustis <avgoustis.george.mail@gmail.com>
2018-10-13 18:54:28 +0300

    major bug fixing, apologies for previous commit to the entire community of the repo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export default [
   'When in doubt, use a popup.',
   'Let’s debug it directly in production.',
   'Just ship it already.',
-  'The leak is minor, we\'ll just add more RAM later.',
-  'Looks like it\'s all working fine',
-  'We\'ll fix it later',
+  'The leak is minor, we’ll just add more RAM later.',
+  'Looks like it’s all working fine',
+  'We’ll fix it later',
 ];
